3d472dd3c77b23be53de6d7f9b70d5ac1312ff06
Sherouk Mhmoud Mohamed <104649605+sherouk6@users.noreply.github.com>
2022-11-05 00:12:54 +0300

    1

--- home/get_pet_labels_hints.py
+++ home/get_pet_labels_hints.py
@@ -20,16 +20,18 @@
 # Imports python modules
 from os import listdir
 
-# TODO 2: EDIT and ADD code BELOW to do the following that's stated in the 
-#       comments below that start with "TODO: 2" for the get_pet_labels function 
-#       Please be certain to replace None in the return statement with 
+# TODO 2: EDIT and ADD code BELOW to do the following that's stated in the
+#       comments below that start with "TODO: 2" for the get_pet_labels function
+#       Please be certain to replace None in the return statement with
 #       results_dic dictionary that you create with this function
-# 
+#
+
+
 def get_pet_labels(image_dir):
     """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
+    Creates a dictionary of pet labels (results_dic) based upon the filenames
+    of the image files. These pet image labels are used to check the accuracy
+    of the labels that are returned by the classifier function, since the
     filenames of the images contain the true identity of the pet in the image.
     Be sure to format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
@@ -38,48 +40,55 @@ def get_pet_labels(image_dir):
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      results_dic - Dictionary with 'key' as image filename and 'value' as a
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
     # Creates list of files in directory
     in_files = listdir(image_dir)
-    
+
     # Processes each of the files to create a dictionary where the key
     # is the filename and the value is the picture label (below).
- 
+
     # Creates empty dictionary for the results (pet labels, etc.)
     results_dic = dict()
-   
+
     # Processes through each file in the directory, extracting only the words
     # of the file that contain the pet image label
     for idx in range(0, len(in_files), 1):
-       
-       # Skips file if starts with . (like .DS_Store of Mac OSX) because it 
-       # isn't an pet image file
-       if in_files[idx][0] != ".":
-           
-           # Creates temporary label variable to hold pet label name extracted 
-           pet_label = ""
 
-           # TODO: 2a. BELOW REPLACE pass with CODE that will process each 
-           #          filename in the in_files list to extract the dog breed 
-           #          name from the filename. Recall that each filename can be
-           #          accessed by in_files[idx]. Be certain to place the 
-           #          extracted dog breed name in the variable pet_label 
-           #          that's created as an empty string ABOVE
-           pass
+        # Skips file if starts with . (like .DS_Store of Mac OSX) because it
+        # isn't an pet image file
+        if [idx][0] != ".":
 
-           # If filename doesn't already exist in dictionary add it and it's
-           # pet label - otherwise print an error message because indicates 
-           # duplicate files (filenames)
-           if in_files[idx] not in results_dic:
-              results_dic[in_files[idx]] = [pet_label]
-              
-           else:
-               print("** Warning: Duplicate files exist in directory:", 
-                     in_files[idx])
- 
+            # Creates temporary label variable to hold pet label name extracted
+            pet_label = ""
+
+            # TODO: 2a. BELOW REPLACE pass with CODE that will process each
+            #          filename in the in_files list to extract the dog breed
+            #          name from the filename. Recall that each filename can be
+            #          accessed by in_files[idx]. Be certain to place the
+            #          extracted dog breed name in the variable pet_label
+            #          that's created as an empty string ABOVE
+
+            #remove any of these characters "_123456789" from pet label and replace any "_" to " " to give
+            #appropriate pet label
+            pet_label_old = in_files[idx].lower().split("_")
+            for word in pet_label_old:
+               if word.isalpha():
+                  pet_label += word + " "
+            pet_label = pet_label.strip()
+
+            # If filename doesn't already exist in dictionary add it and it's
+            # pet label - otherwise print an error message because indicates
+            # duplicate files (filenames)
+            if in_files[idx] not in results_dic:
+                results_dic[in_files[idx]] = [pet_label]
+
+            else:
+                print("** Warning: Duplicate files exist in directory:",
+                      in_files[idx])
+
     # TODO 2b. Replace None with the results_dic dictionary that you created
     # with this function
-    return None
+    return results_dic
--- home/run_models_batch.bat
+++ home/run_models_batch.bat
@@ -1,16 +1,14 @@
-@echo off
-REM */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.bat
-REM                                                                             
-REM PROGRAMMER: Jennifer S.
-REM DATE CREATED: 02/08/2018
-REM REVISED DATE: 04/23/2018 - revised run_models_batch.sh to run on 
-REM                            windows OS using bat and Anaconda Prompt 
-REM PURPOSE: Runs all three models to test which provides 'best' solution.
-REM          Please note output from each run has been piped into a text file.
-REM
-REM Usage: run_models_batch.bat  -- will run program from commandline on Window OS
-REM 
-@echo on
+#!/bin/sh
+# */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
+#
+# PROGRAMMER: Jennifer S.
+# DATE CREATED: 02/08/2018
+# REVISED DATE: 02/27/2018  -
+# PURPOSE: Runs all three models to test which provides 'best' solution.
+#          Please note output from each run has been piped into a text file.
+#
+# Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
+#
 python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
 python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
 python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
